7000ea12c374b8a8b465811e9fe2ced006d62710
Ton0703 <44728377+Ton0703@users.noreply.github.com>
2020-06-06 21:08:12 +0800

    infinite-scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Item from "./item";
+import "./App.css";
 
 function App() {
+  const [list, setLists] = useState(Array.from(Array(30).keys(), (n) => n + 1));
+  const [fetchData, setFetchData] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  useEffect(() => {
+    if (fetchData === false) return;
+    fetchDataList();
+  }, [fetchData]);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setFetchData(true);
+  };
+  const fetchDataList = () => {
+    setTimeout(() => {
+      setLists((preList) => [
+        ...preList,
+        ...Array.from(Array(20).keys(), (n) => n + preList.length + 1),
+      ]);
+      setFetchData(false);
+    }, 2000);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((item) => (
+        <Item value={item} key={item} />
+      ))}
+      {fetchData && <div>Loading</div>}
     </div>
   );
 }
---
+++ src/item.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function item({value}) {
+    return (
+        <div className='item'>
+            {value}
+        </div>
+    )
+}
+
+export default item
